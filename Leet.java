38effd2c555c90e0751aa44808be9083ddb3d141
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2022-08-18 12:49:14 +0000

    Count sort

--- Leet.java
+++ Leet.java
@@ -2,8 +2,67 @@ import java.util.Arrays;
 
 public class Leet {
     public static void main(String[] args) {
-        int[] input = { 9, 9, 9 };
-        System.out.println(Arrays.toString(plusOne(input)));
+        int[] input = { 4, 10, 3, 2, 1, 7, -17 };
+        countingSort(input);
+    }
+
+    public static void countingSort(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num < min)
+                min = num;
+            if (num > max)
+                max = num;
+        }
+
+        int[] count = new int[(max - min) + 2];
+
+        for (int num : nums) {
+            count[num - min]++;
+        }
+
+        for (int i = 0; i < count.length - 1; i++) {
+            count[i + 1] = count[i + 1] + count[i];
+        }
+
+        for (int i = count.length - 1; i > 0; i--) {
+            count[i] = count[i - 1];
+        }
+
+        count[0] = 0;
+
+        int[] output = new int[nums.length];
+
+        for (int num : nums) {
+            output[count[num - min]] = num;
+            count[num - min]++;
+        }
+
+        System.out.println(Arrays.toString(output));
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            sum += Math.min(nums[i], nums[i + 1]);
+        }
+        return sum;
+    }
+
+    public static void reverseString(char[] s) {
+        if (s.length >= 2) {
+            int p1 = 0, p2 = s.length - 1;
+            while (p1 < p2) {
+                char temp = s[p2];
+                s[p2] = s[p1];
+                s[p1] = temp;
+                p1++;
+                p2--;
+            }
+        }
+
     }
 
     public static int[] plusOne(int[] digits) {
